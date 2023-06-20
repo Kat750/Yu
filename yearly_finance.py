2ad0f5617f970b48fe34bf7dcbb1dd5d86d84d10
Kat <124098744+Kat750@users.noreply.github.com>
2023-06-20 12:25:22 +0100

    table for each year, totals from fundraiser list

--- yearly_finance.py
+++ yearly_finance.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import psycopg2
 from sqlalchemy import create_engine
+import matplotlib.pyplot as plt
+
+
 print('test')
 
 #read in excel file income_alias with six sheets
@@ -36,9 +39,36 @@ column_names = list(df2017_18.columns)
 print(column_names)
 
 #how much in total is raised each year via Fundraiser
+
+dataframes = [df2017_18, df2018_19, df2019_20, df2020_21, df2021_22]
+years = ['2017-18', '2018-19', '2019-20', '2020-21', '2021-22']
+totals = []
+
+for year, df in zip(years,dataframes):
+    total_fundraiser = df.iloc[:, 2].sum()  # Assuming 'Donation via Fundraiser' is the third column (index 2)
+    totals.append(total_fundraiser)
+    print(f'Total Fundraiser for {year}: {total_fundraiser}')
+
+# Use the total fundraiser values stored in the list called 'totals'
+years = ['2017-18', '2018-19', '2019-20', '2020-21', '2021-22']
+
+# Create a pandas DataFrame from the years and totals
+df_years_totals = pd.DataFrame({'Year': years, 'Total Fundraiser': totals})
+
+# Plotting the data
+df_years_totals.plot(x='Year', y='Total Fundraiser', kind='bar', rot=0)
+
+# Adding labels and title
+plt.xlabel('Year')
+plt.ylabel('Total Fundraiser')
+plt.title('Total Fundraiser by Year')
+
+# Display the plot
+plt.show()
 #how much in total is raised each year Direct
 #how much in total is raised each year
 #which source IDs donate every year, then via Fundraiser or Direct
 #list top largest donator source ID in descending order for each year, what proportion of these are via Fundraiser, are any of these in multible years
+
 
 
